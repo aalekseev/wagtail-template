4a5d9f05aff22bbded9967852e40771dbee07a9c
Anton Alekseev <hi@aalekseev.me>
2022-10-10 15:37:13 +0300

    feat[Accounts]: Port back Django account model
    We are using the custom user model, however at some point we decided to get rid of the `first_name` and `last_name` fields. Now this does not work well with Wagtail setup - the user profile editing form does not work. Instead of customizing the admin forms as well, we decided that it is simplier to just add these two fields back and have a good working system.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,51 +1,55 @@
 from typing import List
 
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):  # pragma: nocover
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
+class CustomUserManager(BaseUserManager):
+    # Mostly copied from django.contrib.auth.models.UserManager
 
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        now = timezone.now()
         if not email:
-            raise ValueError("Users require an email field")
+            raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields,
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        if extra_fields.get("is_active") is not True:
-            raise ValueError("Superuser must have is_active=True.")
-
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, True, True, **extra_fields)
 
 
-class CustomUser(AbstractUser):
-    username = None  # type: ignore
-    email = models.EmailField(_("email address"), unique=True)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(
+        verbose_name=_("email address"), max_length=254, unique=True
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+
+    is_staff = models.BooleanField(_("staff status"), default=False)
     is_active = models.BooleanField(
         _("active"),
         default=False,
@@ -55,11 +59,23 @@ class CustomUser(AbstractUser):
             "Unselect this instead of deleting accounts."
         ),
     )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS: List[str] = []
 
-    objects = CustomUserManager()  # type: ignore
+    objects = CustomUserManager()
 
-    def __str__(self):  # pragma: no cover
-        return self.email
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip() or self.email
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name or self.email
+
+    def __str__(self):
+        return self.get_full_name()
