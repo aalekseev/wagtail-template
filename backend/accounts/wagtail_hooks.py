f646a893741f30d11219e54e2af744b3738feaec
Anton Alekseev <hi@aalekseev.me>
2022-10-11 19:51:43 +0300

    chore(lint): Reformat Python codebase with black and isort

--- backend/accounts/wagtail_hooks.py
+++ backend/accounts/wagtail_hooks.py
@@ -8,17 +8,20 @@ from django.utils.safestring import mark_safe
 from wagtail.admin.ui.components import Component
 from wagtail.core import hooks
 
+
 class WelcomePanel(Component):
     order = 50
 
     def render_html(self, parent_context):
-        return mark_safe("""
-        <section class="panel summary nice-padding">
-          <h3>No, but seriously -- welcome to the admin homepage.</h3>
-        </section>
-        """)
+        return mark_safe(
+            """
+            <section class="panel summary nice-padding">
+              <h3>No, but seriously -- welcome to the admin homepage.</h3>
+            </section>
+            """
+        )
 
 
-@hooks.register('construct_homepage_panels')
+@hooks.register("construct_homepage_panels")
 def add_another_welcome_panel(request, panels):
     panels.append(WelcomePanel())
